cf2b2e6a914936ccb966e0aac4508f188d7c5eb3
Juliana Ribeiro <silva.jurib@gmail.com>
2022-10-15 17:24:44 -0300

    feat: add button component

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from './Button'
+
+export default {
+    title: 'Components/Button',
+    component: Button,
+    args: {
+        children: 'Create a account',
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<ButtonProps>
+
+export const Default: StoryObj<ButtonProps> = {
+    argTypes: {
+        asChild: {
+            table: {
+                disable: true
+            }
+        }
+    }
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,8 @@ module.exports = {
       '2xl': 32,
     },
     colors: {
+      black: '#000',
+      white: '#FFF',
       'gray-900': '#121214',
       'gray-800': '#202024',
       'gray-400': '#7C7C8A',
