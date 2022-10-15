7e69c0c39c852acd76872da0bebce11ae9b25206
Juliana Ribeiro <silva.jurib@gmail.com>
2022-10-15 17:43:59 -0300

    fix(button sb): delete size arg

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -6,15 +6,6 @@ export default {
     component: Button,
     args: {
         children: 'Create a account',
-        size: 'md',
-    },
-    argTypes: {
-        size: {
-            options: ['sm', 'md', 'lg'],
-            control: {
-                type: 'inline-radio'
-            }
-        }
     }
 } as Meta<ButtonProps>
 
