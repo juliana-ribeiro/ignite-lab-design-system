2cdbdfddb1e913bde079d41e665613f411c8e939
Juliana Ribeiro <silva.jurib@gmail.com>
2022-10-17 03:00:59 -0300

    feat: add text input component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,20 +13,26 @@ module.exports = {
       '2xl': 32,
     },
     colors: {
+      transparent: 'transparent',
       black: '#000',
       white: '#FFF',
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#9BE1FB',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
+      cyan: {
+        500: '#81D8F7',
+        300: '#9BE1FB',
+      },
     },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
       },
+      maxHeight: ['focus'],
     },
   },
   plugins: [],
